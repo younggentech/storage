c3cdde163bae8744478c7abafe3dcbc2bda9573f
AlexOff <alexov2004@gmail.com>
2021-01-15 16:25:22 +0300

    реализовал put_item для API

--- send_requests.py
+++ send_requests.py
@@ -8,3 +8,15 @@ class storage_api:
     def get_schema(self):
         answer = requests.get("http://"+self.host+":"+self.port+"/scheme")
         return answer.text
+
+    def put_item(self,data):
+        try:
+            answer = requests.post(url="http://"+self.host+":"+self.port,
+                                   json=data)
+            return answer.text
+        except Exception as e:
+            print(e)
+
+
+# a = storage_api("127.0.0.1", "5000")
+# a.put_item([{"uuid": "67568fb7f2c1d06d40450a478863bab1", "destination":["A1"]}])
