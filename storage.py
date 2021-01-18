1aab209283bbb3eca7878879ca8b9d83bfde73fd
AlexOff <alexov2004@gmail.com>
2021-01-18 20:51:39 +0300

    инкапсулировал методы storage.py

--- storage.py
+++ storage.py
@@ -20,7 +20,7 @@ class Storage(RenderStorage):
                 for _cell_num in range(self.width - 1, -1, -1):
                     _cell = self.cells[_block_num][_cell_num]
                     _item = items[_item_num]
-                    _gab = self.check_gabarits(_item, _cell)
+                    _gab = self._check_gabarits(_item, _cell)
                     if not _cell.busy:
                         if _gab:
                             if _cell.size_height+_cell.size_width+_cell.size_depth == _gab:
@@ -29,17 +29,17 @@ class Storage(RenderStorage):
                                 if _cell.merged:
                                     for _merged_cell in _cell.merged_with:
                                         self.easy_find_cell_by_name[_merged_cell].put_to_cell(_item)
-                                self.send_to_db(_item, _cell)
+                                self._send_to_db(_item, _cell)
                                 _item_was_put=True
                                 break
                         else:
-                            self.send_to_remote(_item)
+                            self._send_to_remote(_item)
                 if _item_was_put:
                     break
 
         super(Storage, self).render()
 
-    def check_gabarits(self, _item: Item, _cell):
+    def _check_gabarits(self, _item: Item, _cell):
         """CHECK SIZE OF ITEM"""
         if ((_item.height + _item.width + _item.depth) > 5) or _item.height>2 or _item.width>2 or _item.depth>2:
             return 0
@@ -52,11 +52,11 @@ class Storage(RenderStorage):
         else:
             return 5
 
-    def send_to_remote(self, _item):
+    def _send_to_remote(self, _item):
         """SEND DATA TO REMOTE DB"""
         pass
 
-    def send_to_db(self, param, _cell):
+    def _send_to_db(self, param, _cell):
         """SEND DATA TO MAIN DB"""
         pass
 
