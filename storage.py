dd3cd66eb41fab654d9111c7dea1add3350a5a55
AlexOff <alexov2004@gmail.com>
2021-01-18 21:58:00 +0300

    добавил класс для общения с БД, чтобы соблдать принцип единообязанности

--- storage.py
+++ storage.py
@@ -4,20 +4,36 @@ from render_storage import RenderStorage
 from get_data_from_csv_xls import Item, WayBill
 
 
+class TalkToDB:
+    def __init__(self):
+        pass
+
+    def send_to_db(self, item, cell):
+        pass
+
+    def get_from_db(self, key):
+        pass
+
+    def send_to_remote_db(self, data):
+        pass
+
+
 class Storage(RenderStorage):
     def __init__(self, host, port):
         super().__init__(host, port)
+        self.items_dict = {}
+        self.database_sender = TalkToDB()
 
-    def start_putting(self, way_bill: WayBill):
+    def put(self, way_bill: WayBill):
         """START PROCESS OF PUTTING"""
         data = way_bill.create_item_list()
-        return self.solve_how_to_put(data)
+        return self._solve_how_to_put(data)
 
-    def solve_how_to_put(self, items):
+    def _solve_how_to_put(self, items):
         """SOLVE HOW TO PUT ITEM TO DB"""
         _data_to_send_to_api = []
 
-        for _item_num in range(len(items)-1, -1, -1):
+        for _item_num in range(len(items) - 1, -1, -1):
             _item_was_put = False
             for _block_num in range(self.height - 1, -1, -1):
                 for _cell_num in range(self.width - 1, -1, -1):
@@ -26,20 +42,23 @@ class Storage(RenderStorage):
                     _gab = self._check_gabarits(_item, _cell)
                     if not _cell.busy:
                         if _gab:
-                            if _cell.size_height+_cell.size_width+_cell.size_depth == _gab:
+                            if _cell.size_height + _cell.size_width + _cell.size_depth == _gab:
                                 _cell.put_to_cell(_item)
+                                self.items_dict.update({_item.uuid: _item})
 
                                 if _cell.merged:
                                     for _merged_cell in _cell.merged_with:
                                         self.easy_find_cell_by_name[_merged_cell].put_to_cell(_item)
 
                                 _data_to_send_to_api.append({"uuid": _item.uuid,
-                                                             "destination": [_cell.name] if not _cell.merged else _cell.merged_with})
-                                self._send_to_db(_item, _cell)
-                                _item_was_put=True
+                                                             "destination": [
+                                                                 _cell.name] if not _cell.merged else _cell.merged_with})
+                                self.database_sender.send_to_db(_item, _cell)
+
+                                _item_was_put = True
                                 break
                         else:
-                            self._send_to_remote(_item)
+                            self.database_sender.send_to_remote_db(_item)
                 if _item_was_put:
                     break
         resp = json.loads(self.put_item_api(_data_to_send_to_api))
@@ -51,7 +70,7 @@ class Storage(RenderStorage):
 
     def _check_gabarits(self, _item: Item, _cell):
         """CHECK SIZE OF ITEM"""
-        if ((_item.height + _item.width + _item.depth) > 5) or _item.height>2 or _item.width>2 or _item.depth>2:
+        if ((_item.height + _item.width + _item.depth) > 5) or _item.height > 2 or _item.width > 2 or _item.depth > 2:
             return 0
         elif _item.height == 1 and _item.width == 1 and _item.depth == 1:
             return 3
@@ -62,12 +81,8 @@ class Storage(RenderStorage):
         else:
             return 5
 
-    def _send_to_remote(self, _item):
-        """SEND DATA TO REMOTE DB"""
-        pass
-
-    def _send_to_db(self, param, _cell):
-        """SEND DATA TO MAIN DB"""
+    def get(self, uuid: str):
+        """GET POSITION FROM STORAGE"""
         pass
 
 
@@ -85,7 +100,8 @@ for i in wb.create_item_list():
     print(i.__dict__)
 
 print()
-print(tr.start_putting(wb))
+print(tr.put(wb))
+print(tr.items_dict)
 print()
 
 for i in tr.cells:
